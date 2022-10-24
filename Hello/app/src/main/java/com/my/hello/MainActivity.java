0187c99e587b13490503bd4683ab620506214a8b
Cyan <2wnaud@gmail.com>
2022-10-24 14:01:21 +0900

    버튼 추가하기

--- Hello/app/src/main/java/com/my/hello/MainActivity.java
+++ Hello/app/src/main/java/com/my/hello/MainActivity.java
@@ -1,7 +1,12 @@
 package com.my.hello;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButton1Clicked(View v) {
+        // 메세지 노출
+        Toast.makeText(this, "확인1 버튼이 눌렸어요.", Toast.LENGTH_LONG).show();
+    }
+
+    public void onButton2Clicked(View v) {
+        // 웹 브라우저를 열고 네이버 사이트로 이동
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+        startActivity(intent);
+    }
+
+    public void onButton3Clicked(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-1000-1000"));
+        startActivity(intent);
     }
 }
