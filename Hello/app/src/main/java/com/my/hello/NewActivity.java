20537dee5a9968937053f389cc6f5631b92c310c
Cyan <2wnaud@gmail.com>
2022-10-25 13:56:12 +0900

    안드로이드 스튜디오 구조 확인

--- Hello/app/src/main/java/com/my/hello/NewActivity.java
+++ Hello/app/src/main/java/com/my/hello/NewActivity.java
@@ -1,7 +1,5 @@
 package com.my.hello;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -20,16 +18,5 @@ public class NewActivity extends AppCompatActivity {
         // 메세지 노출
         Toast.makeText(this, "돌아가기 버튼이 눌렸어요.", Toast.LENGTH_LONG).show();
         finish();
-    }
-
-    public void onButton2Clicked(View v) {
-        // 웹 브라우저를 열고 네이버 사이트로 이동
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
-        startActivity(intent);
-    }
-
-    public void onButton3Clicked(View v) {
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-1000-1000"));
-        startActivity(intent);
     }
 }
