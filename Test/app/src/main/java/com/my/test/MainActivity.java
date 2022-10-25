5a8dc8c5e45e4622238a47beaa45faf543ea05d4
Cyan <2wnaud@gmail.com>
2022-10-25 16:01:59 +0900

    LinearLayout Practice

--- Test/app/src/main/java/com/my/test/MainActivity.java
+++ Test/app/src/main/java/com/my/test/MainActivity.java
@@ -3,12 +3,25 @@ package com.my.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.Button); // id가 Button인 뷰를 button 변수에 대입
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
